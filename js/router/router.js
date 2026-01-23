0830e48b811b32131db24fd997f28572ddc5973c
DESKTOP-091MBB9\Garison4ik <staskalyn8754@gmail.com>
2026-01-23 11:12:47 +0200

    crated page 404

--- js/router/router.js
+++ js/router/router.js
@@ -3,15 +3,32 @@ const container = document.getElementById('content');
 
 async function loadPage() {
     const fileName = window.location.hash.replace('#','') || 'home';
-    const content = await (await fetch(`pages/${fileName}.html`)).text();
-
-    container.innerHTML = content;
-    document.dispatchEvent(new CustomEvent('contentUpdated'));
-    
     
     try {
+        const response = await fetch(`pages/${fileName}.html`);
+
+        if(!response.ok){
+            throw new Error('Page not found');
+        }
+
+        const content = await (response).text();
+        container.innerHTML = content;
+        document.dispatchEvent(new CustomEvent('contentUpdated'));
+        loadPageScript(fileName);
+
+    } catch (error) {
+        const content = await (await fetch(`pages/404.html`)).text();  
+        container.innerHTML = content;
+        document.dispatchEvent(new CustomEvent('contentUpdated'));
+    }
+   
+    
+}
+
+async function loadPageScript(fileName) {
+    try {
         await import(`../pages/${fileName}.js`);
-    } catch (e) {
+    } catch (error) {
         console.log(`No JS module for page: ${fileName}`);
     }
 }
